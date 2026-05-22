2ad29286e6a18d1d3ab0d03f2d08f81fb2d1de16
Prince <princemungra1340@gmail.com>
2026-05-22 17:45:16 +0530

    update page

--- app/API/api.js
+++ app/API/api.js
@@ -1,9 +1,31 @@
 import axios from "axios";
 
-
 const API = axios.create({
+
     baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
+API.interceptors.request.use(
+
+    (config) => {
+
+        if (typeof window !== "undefined") {
+
+            const token = localStorage.getItem("token");
+
+            if (token) {
+
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+
+        return config;
+    },
+
+    (error) => {
+
+        return Promise.reject(error);
+    }
+);
 
 export default API;
